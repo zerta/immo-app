565a75427c7e57d0a6308c4c692a444d273fd35f
ZERTA <zerta.kamal@gmail.com>
2015-10-19 22:15:49 +0100

    Publish.

---
+++ demo/src/main/java/com/bounya/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.bounya.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+	@RequestMapping("/")
+public String home(){
+	return "Hello, KHARBOUCH";
+}
+}
