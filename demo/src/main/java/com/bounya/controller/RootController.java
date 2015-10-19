af3a8c3f8aafbd4e144d883ab75f50cfd4a105bb
MAK <MAK@MAK-PC>
2015-10-19 23:37:30 +0200

    hana

--- demo/src/main/java/com/bounya/controller/RootController.java
+++ demo/src/main/java/com/bounya/controller/RootController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 	@RequestMapping("/")
 public String home(){
-	return "Hello, KAMAL";
+	return "Hello, KMAL";
 	
 }
 }
